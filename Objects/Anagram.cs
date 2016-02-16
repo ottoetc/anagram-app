a9995c8fe897b96cf4d1d34dbab41641093eade8
Nathan <ottoetc@gmail.com>
2016-02-16 11:27:02 -0800

    add first tests

---
+++ Objects/Anagram.cs
@@ -0,0 +1,17 @@
+namespace Anagram
+{
+  public class Anagram
+  {
+    public bool InputIsSame(string input1, string input2)
+    {
+      if (input1 == input2)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
--- Tests/AnagramTest.cs
+++ Tests/AnagramTest.cs
@@ -4,9 +4,16 @@ namespace Anagram
   public class AnagramTest
   {
     [Fact]
-    public void MethodName()
+    public void AnagramTest_StringsMatchTrue()
     {
-
+      var testAnagram = new Anagram();
+      Assert.Equal(true, testAnagram.InputIsSame("n", "n"));
+    }
+    [Fact]
+    public void AnagramTest_StringsMatchFalse()
+    {
+      var testAnagram = new Anagram();
+      Assert.Equal(false, testAnagram.InputIsSame("n", "a"));
     }
   }
 }
